3f518f4ca55b18a99cbc4dc29ffb01a2b78ba3ad
ADITYAVLOGGER <coderaditya8661@gmail.com>
2026-07-25 20:36:45 +0530

    Update server.js

--- server.js
+++ server.js
@@ -279,10 +279,11 @@ Question: ${question}
     }
 });
 
-app.get("/", (req, res) => {
-    res.send("Server is alive 🚀")
-})
+const PORT = process.env.PORT || 3000
 
+app.listen(PORT, () => {
+    console.log(`🚀 Server running on port ${PORT}`)
+})
 
 // ---------------- SERVER ----------------
 app.listen(3000, () => {
